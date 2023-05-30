3c5b9b310b59a201094a01a42128f661a0aa22d4
Tran Quang Loc <quangloctran99@gmail.com>
2023-05-30 10:49:14 +0700

    Add preprocess step to render-ts-docs

---
+++ scripts/utils.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+
+// https://stackoverflow.com/a/48032528
+export async function replaceAsync(
+    str: string,
+    regex: string | RegExp,
+    asyncFn: (match: string, ...rest: any[]) => Promise<string>
+) {
+    const promises: Promise<string>[] = [];
+    str.replace(regex, (match, ...args) => {
+        const promise = asyncFn(match, ...args);
+        promises.push(promise);
+        return '';
+    });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift()!);
+}
+
+export function resolveIncludePath(mainFilePath: string, includedFilePath: string) {
+    if (!includedFilePath.startsWith('.')) {
+        return path.resolve('./' + includedFilePath);
+    }
+    const mainFileRelativeDir = path.dirname(mainFilePath);
+    return path.resolve(mainFileRelativeDir, includedFilePath);
+}
